1c17747806ef2cc24f630d1aad5a5cb8969b61e1
William Stein <williamstein92@gmail.com>
2016-05-11 19:14:27 -0400

    generators

--- gen.py
+++ gen.py
@@ -7,3 +7,27 @@ def file(filename, struct, *data):
         for line in lines:
             f.write(struct.format(*line))
 
+def cube(n=2):
+    X,Y,Z = [range(2)]*3
+    return [[[z*n*n + y*n + x
+        for x in X]
+            for y in Y]
+                for z in Z]
+
+def graph(n=2):
+    def ifapp(a,x,y,z):
+        try:
+            E.append((a,C[z][y][x]))
+        except:
+            return
+
+    C = cube(n)
+    E = []
+    for z,Z in enumerate(C):
+        for y,Y in enumerate(Z):
+            for x,X in enumerate(Y):
+                ifapp(X,x+1,y,z)
+                ifapp(X,x,y+1,z)
+                ifapp(X,x,y,z+1)
+    return E
+
